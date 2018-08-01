248a836a122dba25b9deb96a5a4032e96e000ee9
fernandes.gilroy@gmail.com <fernandes.gilroy@gmail.com>
2018-08-01 01:21:41 +0000

    template

---
+++ public/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var _jsx = function () { var REACT_ELEMENT_TYPE = typeof Symbol === "function" && Symbol.for && Symbol.for("react.element") || 0xeac7; return function createRawReactElement(type, props, key, children) { var defaultProps = type && type.defaultProps; var childrenLength = arguments.length - 3; if (!props && childrenLength !== 0) { props = {}; } if (props && defaultProps) { for (var propName in defaultProps) { if (props[propName] === void 0) { props[propName] = defaultProps[propName]; } } } else if (!props) { props = defaultProps || {}; } if (childrenLength === 1) { props.children = children; } else if (childrenLength > 1) { var childArray = Array(childrenLength); for (var i = 0; i < childrenLength; i++) { childArray[i] = arguments[i + 3]; } props.children = childArray; } return { $$typeof: REACT_ELEMENT_TYPE, type: type, key: key === undefined ? null : '' + key, ref: null, props: props, _owner: null }; }; }();
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+//import CustomText from './customText.jsx'
+
+console.log("in app.js");
+
+var _ref = _jsx('p', {}, void 0, 'Hello world');
+
+var HelloWorld = function (_React$Component) {
+    _inherits(HelloWorld, _React$Component);
+
+    function HelloWorld() {
+        _classCallCheck(this, HelloWorld);
+
+        return _possibleConstructorReturn(this, (HelloWorld.__proto__ || Object.getPrototypeOf(HelloWorld)).apply(this, arguments));
+    }
+
+    _createClass(HelloWorld, [{
+        key: 'render',
+        value: function render(props) {
+            console.log();
+            return _jsx('div', {}, void 0, _jsx('h1', {}, void 0, 'Hello ', this.props.name, '!!!'), _ref);
+        }
+    }]);
+
+    return HelloWorld;
+}(_react2.default.Component);
+
+_reactDom2.default.render(_jsx(HelloWorld, {
+    name: "Gilroy Fernandes"
+}), document.getElementById('root'));
---
+++ src/customText.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CustomText = (props) => {
+    return <p>Custom Text !!!!! {props.text}</p>;
+}
+
+export default CustomText;
